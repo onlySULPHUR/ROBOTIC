02274a1cbd1e0aed6131b996f8bc447fce0b6821
Sulphur <60334841+onlySULPHUR@users.noreply.github.com>
2021-09-10 11:55:36 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,5 @@
+mkdir /data/data/com.termux/files/home/ROBOTIC/pytransform
+mv /data/data/com.termux/files/home/ROBOTIC/{*init*,*.so} /data/data/com.termux/files/home/ROBOTIC/pytransform/
+chmod -Rf 777 /data/data/com.termux/files/home/ROBOTIC
+clear
+echo -e "\e[93mSet Up Completed And You Are Now Ready To Use ROBOTIC Antiban"
