6796452a83e289d04e93397185dd35921ed3e2ca
Sulphur <60334841+onlySULPHUR@users.noreply.github.com>
2021-09-23 14:47:36 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 mkdir /data/data/com.termux/files/home/ROBOTIC/pytransform
 mv /data/data/com.termux/files/home/ROBOTIC/{*init*,*.so} /data/data/com.termux/files/home/ROBOTIC/pytransform/
 chmod -Rf 777 /data/data/com.termux/files/home/ROBOTIC
+rm -rf /data/data/com.termux/files/home/ROBOTIC/{key,GM,GL,version}
 clear
 echo -e "\e[93mSet Up Completed And You Are Now Ready To Use ROBOTIC Antiban"
-rm -rf /data/data/com.termux/files/home/ROBOTIC/{key,GM,GL,version}
